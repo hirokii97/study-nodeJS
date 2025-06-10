ee74cfccc57492ccfcdec39def82231b27f7f289
hirokii97 <basket100713hiroki.ch@gmail.com>
2025-06-10 21:20:44 +0900

    apiの設定

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const server = createServer((req, res) => {
   if (req.url === "/hello") {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("hello world");
+  } else if (req.url === "/api/user") {
+    const user = { id: 1, name: "Hiroki" };
+    res.writeHead(200, { "Content-type": "text/json" });
+    res.end(JSON.stringify(user));
   } else {
-    res.writeHead(404, { "content-Type": "text/plain" });
+    res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("bye");
   }
 });
